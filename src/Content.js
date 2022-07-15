31cbef88fb3a17d0568f3749a8eef1580d7073a0
Brunopjunq <bruno_junq@poli.ufrj.br>
2022-07-15 14:56:29 -0300

    Add LeftContent

---
+++ src/Content.js
@@ -0,0 +1,11 @@
+import LeftContent from './LeftContent';
+import SideBar from './SideBar';
+
+export default function Content() {
+    return (
+        <div class="corpo">
+            <LeftContent />
+            <SideBar />
+        </div>
+    )
+}

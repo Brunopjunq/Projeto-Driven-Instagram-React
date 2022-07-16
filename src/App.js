ccec1df92094bbcb067084b25e56ed06b0981209
Brunopjunq <bruno_junq@poli.ufrj.br>
2022-07-16 16:54:43 -0300

    Add BottomMobile

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import NavBar from './NavBar';
 import Content from './Content';
-
+import BottomMobile from './BottomMobile';
 
 export default function App() {
     return (
         <div>
             <NavBar />
             <Content />
-            
+            <BottomMobile />
         </div>
     )
 }
--- src/Content.js
+++ src/Content.js
@@ -6,7 +6,6 @@ export default function Content() {
         <div class="corpo">
             <LeftContent />
             <SideBar />
-            <BottomMobile />
         </div>
     )
 }
